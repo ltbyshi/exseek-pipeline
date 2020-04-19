cc6332e5ed7e69de4ed6d60bfdf6cc530541cb34
vbox <vbox@ubuntu.vbox>
2020-04-19 21:52:39 +0800

    udpate setup.py

--- setup.py
+++ setup.py
@@ -12,10 +12,11 @@ setup(
     version='1.0.0',
     scripts=scripts,
     packages=['exseek'],
+    package_data={'exseek': ['config/*.yaml', 'scripts/*', 'singularity/*', 'templates/*', 'snakefiles/*', 'snakefiles/rules/*', 'snakefiles/scripts/*']},
     include_package_data=True,
     install_requires=requirements,
     url='https://github.com/lulab/exseek',
     description='exSEEK - a pipeline for analysis of exRNA sequencing data',
     zip_safe=False,
-    data_files=[("", ['LICENSE', 'requirements.txt'])]
+    data_files=[('', ['LICENSE', 'requirements.txt'])]
 )
