c049257575103579d45b70b449509d34153787e0
vbox <vbox@ubuntu.vbox>
2020-04-19 20:52:01 +0800

    merge machine_learning.yaml into default_config.yaml

--- exseek/scripts/machine_learning.py
+++ exseek/scripts/machine_learning.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 import argparse, sys, os, errno
 import logging
-logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] [%(levelname)s] %(name)s: %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] [%(levelname)s] %(name)s: %(message)s')
 logger = logging.getLogger('machine_learning')
 import json
 from tqdm import tqdm
